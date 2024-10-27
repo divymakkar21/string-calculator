4a5dbafc91a3a1b72977f7051aec5eeb190abe80
divymakkar21 <divy.makkar@zetapp.in>
2024-10-27 19:59:43 +0530

    Added test cases

---
+++ stringCalculator.test.js
@@ -0,0 +1,30 @@
+// stringCalculator.test.js
+const add = require('./stringCalculator');
+
+test('should return 0 for an empty string', () => {
+    expect(add("")).toBe(0);
+});
+
+test('should return the number for a single number', () => {
+    expect(add("1")).toBe(1);
+});
+
+test('should return sum for two numbers separated by a comma', () => {
+    expect(add("1,5")).toBe(6);
+});
+
+test('should handle multiple numbers', () => {
+    expect(add("1,2,3")).toBe(6);
+});
+
+test('should handle new lines as delimiters', () => {
+    expect(add("1\n2,3")).toBe(6);
+});
+
+test('should throw an error for negative numbers', () => {
+    expect(() => add("1,-2,3")).toThrow("Negative numbers not allowed: -2");
+});
+
+test('should list all negative numbers in the error message', () => {
+    expect(() => add("1,-2,-3")).toThrow("Negative numbers not allowed: -2, -3");
+});
